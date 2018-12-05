283d06e980291f17ffa4dd130aa6829e99fa3251
rexim <reximkut@gmail.com>
2018-12-06 03:26:08 +0700

    (#103) Introduce ProjectConfig

---
+++ project_config.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+)
+
+type TitleConfig struct {
+}
+
+func (titleConfig *TitleConfig) Transform(title string) string {
+	// TODO: TitleConfig.Transform() is not implemented
+	return title
+}
+
+type ProjectConfig struct {
+	Title *TitleConfig
+}
+
+func NewProjectConfig(filePath string) (*ProjectConfig, error) {
+	// TODO: NewProjectConfig() is not implemented
+	return &ProjectConfig {
+		Title: &TitleConfig{},
+	}, nil
+}
