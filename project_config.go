5e4958e819f0004657f53b0b6589135c0333647a
rexim <reximkut@gmail.com>
2018-12-12 00:52:55 +0700

    (#107) Implement NewProjectConfig

--- project_config.go
+++ project_config.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"regexp"
+	"os"
+	"fmt"
+	"gopkg.in/yaml.v2"
 )
 
 // TransformRule defines a title transformation rule
@@ -30,6 +33,11 @@ func (titleConfig *TitleConfig) Transform(title string) string {
 	return title
 }
 
+func NewTitleConfig(v interface{}) (*TitleConfig, error) {
+	// TODO: NewTitleConfig is not implemented
+	return nil, nil
+}
+
 // ProjectConfig contains the project level configuration
 type ProjectConfig struct {
 	Title *TitleConfig
@@ -37,8 +45,38 @@ type ProjectConfig struct {
 
 // NewProjectConfig constructs the ProjectConfig from a YAML file
 func NewProjectConfig(filePath string) (*ProjectConfig, error) {
-	// TODO(#107): NewProjectConfig() is not implemented
-	return &ProjectConfig{
-		Title: &TitleConfig{},
+	if stat, err := os.Stat(filePath); os.IsNotExist(err) || stat.IsDir() {
+		return &ProjectConfig{
+			Title: &TitleConfig{
+				TransformRules: []TransformRule{},
+			},
+		}, nil
+	}
+
+	configFile, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer configFile.Close()
+
+	yamlDecoder := yaml.NewDecoder(configFile)
+	v := make(map[string]interface{})
+	err = yamlDecoder.Decode(&v)
+	if err != nil {
+		return nil, err
+	}
+
+	titleV, ok := v["title"]
+	if !ok {
+		return nil, fmt.Errorf("%s doesn't have the `title` section", filePath)
+	}
+
+	titleConfig, err := NewTitleConfig(titleV)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ProjectConfig {
+		Title: titleConfig,
 	}, nil
 }
